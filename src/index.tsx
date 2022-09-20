03e0f5e170d7dee456150744161ef459d5b4f90d
fire2124 <filip.reichl@gmail.com>
2022-09-20 13:25:51 +0200

    // for netlify

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
 
 AOS.init({ duration: 3000, once: true });
 
